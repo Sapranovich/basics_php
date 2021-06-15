397933c62212b6bc15a815f7bb9c8033eabeb180
Sapranovich Andrey <SapranovichAndrey@yandex.ru>
2021-06-15 22:27:02 +0300

    feat: add basic routing

--- reg.php
+++ reg.php
@@ -1,3 +1,5 @@
+<?php include("path.php");?>
+
 <html lang="en">
 <head>
     <!-- Required meta tags -->
@@ -19,35 +21,7 @@
 </head>
 <body>
 
-<header class="container-fluid">
-    <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <h1>
-                    <a href="/">My blog</a>
-                </h1>
-            </div>
-            <nav class="col-8">
-                <ul>
-                    <li><a href="#">Главная</a> </li>
-                    <li><a href="#">О нас</a> </li>
-                    <li><a href="#">Услуги</a> </li>
-
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-user"></i>
-                            Кабинет
-                        </a>
-                        <ul>
-                            <li><a href="#">Админ панель</a> </li>
-                            <li><a href="#">Выход</a> </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+<?php include("app/include/header.php");?>
 <!-- END HEADER -->
 <!-- FORM -->
 <div class="container reg_form">
@@ -82,71 +56,7 @@
 </div>>
 <!-- END FORM -->
 
-<!-- footer -->
-<div class="footer container-fluid">
-    <div class="footer-content container">
-        <div class="row">
-            <div class="footer-section about col-md-4 col-12">
-                <h3 class="logo-text">Мой блог</h3>
-                <p>
-                    Мой блог - это блог сделаннный с целью обучения аудитории на платформе
-                    YouTube и заработка доволнительной кармы)).
-                </p>
-                <div class="contact">
-                    <span><i class="fas fa-phone"></i> &nbsp; 123-456-789</span>
-                    <span><i class="fas fa-envelope"></i> &nbsp; info@myblog.com</span>
-                </div>
-                <div class="socials">
-                    <a href="#"><i class="fab fa-facebook"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-youtube"></i></a>
-                </div>
-            </div>
-
-            <div class="footer-section links col-md-4 col-12">
-                <h3>Quick Links</h3>
-                <br>
-                <ul>
-                    <a href="#">
-                        <li>События</li>
-                    </a>
-                    <a href="#">
-                        <li>Команда</li>
-                    </a>
-                    <a href="#">
-                        <li>Упражнения</li>
-                    </a>
-                    <a href="#">
-                        <li>Галлерея</li>
-                    </a>
-                    <a href="#">
-                        <li>Что-то еще</li>
-                    </a>
-                </ul>
-            </div>
-
-            <div class="footer-section contact-form col-md-4 col-12">
-                <h3>Контакты</h3>
-                <br>
-                <form action="index.html" method="post">
-                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
-                    <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
-                    <button type="submit" class="btn btn-big contact-btn">
-                        <i class="fas fa-envelope"></i>
-                        Отправить
-                    </button>
-                </form>
-            </div>
-
-        </div>
-
-        <div class="footer-bottom">
-            &copy; myblog.com | Designed by Andrievskii
-        </div>
-    </div>
-</div>
-<!-- // footer -->
+<?php include("app/include/footer.php");?>
 
 
 <!-- Optional JavaScript; choose one of the two! -->
